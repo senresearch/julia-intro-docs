4cfec2a56093553eb9c68c610346c3d4a816c507
Saunak Sen <sen@uthsc.edu>
2019-10-23 13:53:55 -0500

    Add acknowledgements.

---
+++ notebooks/acknowledgements.jl
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,jl:light
+#     text_representation:
+#       extension: .jl
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.2.1
+#   kernelspec:
+#     display_name: Julia 1.2.0
+#     language: julia
+#     name: julia-1.2
+# ---
+
+# # Acknowledgements
+#
+# - Michael
+# - Funding: GeneNetwork R01 (Sen/Williams, MPI); NIDA P30 (Saba/Williams, MPI)
