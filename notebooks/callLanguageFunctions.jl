6bc501e4db6c1ceb23d089fc517479631ccd50c3
Saunak Sen <sen@uthsc.edu>
2019-10-25 12:15:29 -0500

    Small changes.

--- notebooks/callLanguageFunctions.jl
+++ notebooks/callLanguageFunctions.jl
@@ -199,6 +199,6 @@ rcall(:summary, agren)
 # + {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
 # # Summary
 #
-# ### Calling other lamakes programming in Julia more productive and flexible. 
+# ### Calling other languages makes Julia more productive and flexible.
+# ### It is easy to call R and Python.
 # ### Similar packages exists also for Java, C++ and C.
-#
