f01cf5a6ab21941087ac26904160aa081a2ac084
Saunak Sen <sen@uthsc.edu>
2019-10-25 13:52:44 -0500

    Added comments.

--- notebooks/registration.jl
+++ notebooks/registration.jl
@@ -13,18 +13,25 @@
 #     name: julia-1.2
 # ---
 
+# # Languages used by Julia seminar registrants
+
+# add packages
 using CSV, StatsBase, UnicodePlots
 
+# header for filke
 hdr = ["respType","r","python","matlab","c","java","sas","fortran","other"]
+# read data
 regis = CSV.read("../data/registrants.csv",datarow=5,header=hdr)
+# examine first few lines
 first(regis,10)
 
-# +
+# calculate non-missing responses
 sumnotmissing(x) = length(x) - sum(ismissing.(x))
-
+# proportion of registrants with non-missing (know language)
 prop = sumnotmissing.(eachcol(regis[:,2:(end)]))./size(regis,1)
+# convert to percent
 pct = round.(prop*100)
+# names of languages
 langs = hdr[2:(end)];
-# -
 
 barplot(langs,pct)
